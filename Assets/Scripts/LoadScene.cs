fd0fecd7f0784d5d777d7f6b1aa56b0b240f9315
jlowe11 <81769097+jlowe11@users.noreply.github.com>
2024-06-05 22:11:36 -0700

    Reset scores back to 0 when pressing play

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -8,6 +8,8 @@ public class LoadScene : MonoBehaviour
 
     public void PlayGame()
     {
+        GlobalVariables.player1RoundsWon = 0;
+        GlobalVariables.player2RoundsWon = 0;
         SceneManager.LoadScene(1);
     }
 
